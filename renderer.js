6fe3c0928a0af8e470f5e56712e44642787ecea3
Alex Collins <alex@cloudcade.com>
2019-02-22 10:42:17 -0800

    Move JS Entry Point To Top-Level In Project
     - Makes it so requiring things actually works as expected

--- renderer.js
+++ renderer.js
@@ -1,3 +1,31 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const adminlte = require("adminlte-reactjs");
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+var HeaderBar = require("./src/headerBar").HeaderBar;
+var NavigationMenu = adminlte.NavigationMenu;
+
+class LogViewer extends React.Component {
+  render() {
+    return React.createElement(
+      "div",
+      { className: "wrapper" },
+      React.createElement(HeaderBar, null, null)
+    );
+    /*
+    return (
+      <div className="wrapper">
+        <div className="test">"test1"</div>
+        <div className="content-wrapper">
+          <NavigationMenu />
+        </div>
+      </div>
+    );
+     */
+  }
+}
+
+ReactDOM.render(
+  React.createElement(LogViewer, null, null),
+  document.getElementById("widgets-container")
+);
