9a1510a0f9a39720beba4c0162d66f5091950331
Alex Collins <alex@cloudcade.com>
2019-02-22 11:21:57 -0800

    Finish Header Bar Implementation
     - Basic toggle in-out

--- src/headerBar.js
+++ src/headerBar.js
@@ -20,7 +20,6 @@ exports.HeaderBar = React.createClass({
   },
 
   render: function() {
-    var that = this;
     return React.createElement(
       "header",
       { className: "main-header" },
@@ -40,100 +39,59 @@ exports.HeaderBar = React.createClass({
           { className: "logo-lg" },
           React.createElement("b", null, "GS Log Viewer")
         )
-      )
-    );
-    //   <header className="main-header">
-    //     {/* Logo */}
-    //     <a href="index2.html" className="logo">
-    //       {/* mini logo for sidebar mini 50x50 pixels */}
-    //       <span className="logo-mini">
-    //         <b>A</b>LT
-    //       </span>
-    //       {/* logo for regular state and mobile devices */}
-    //       <span className="logo-lg">
-    //         <b>Admin</b>LTE
-    //       </span>
-    //     </a>
-    //     {/* Header Navbar: style can be found in header.less */}
-    //     <nav className="navbar navbar-static-top" role="navigation">
-    //       {/* Sidebar toggle button*/}
-    //       <a
-    //         href="#"
-    //         className="sidebar-toggle"
-    //         data-toggle="offcanvas"
-    //         role="button"
-    //         onClick={that.pushMenu}
-    //       >
-    //         <span className="sr-only">Toggle navigation</span>
-    //       </a>
-    //       <div className="navbar-custom-menu">
-    //         <ul className="nav navbar-nav">
-    //           {/* Messages: style can be found in dropdown.less*/}
-    //           <HeaderMessages />
-    //           {/* Notifications: style can be found in dropdown.less */}
-    //           <HeaderNotifications />
-    //           {/* Tasks: style can be found in dropdown.less */}
-    //           <HeaderTasks />
-    //           {/* User Account: style can be found in dropdown.less */}
-    //           <li className="dropdown user user-menu">
-    //             <a href="#" className="dropdown-toggle" data-toggle="dropdown">
-    //               <img
-    //                 src="dist/img/user2-160x160.jpg"
-    //                 className="user-image"
-    //                 alt="User Image"
-    //               />
-    //               <span className="hidden-xs">Alexander Pierce</span>
-    //             </a>
-    //             <ul className="dropdown-menu">
-    //               {/* User image */}
-    //               <li className="user-header">
-    //                 <img
-    //                   src="dist/img/user2-160x160.jpg"
-    //                   className="img-circle"
-    //                   alt="User Image"
-    //                 />
-    //                 <p>
-    //                   Alexander Pierce - Web Developer
-    //                   <small>Member since Nov. 2012</small>
-    //                 </p>
-    //               </li>
-    //               {/* Menu Body */}
-    //               <li className="user-body">
-    //                 <div className="col-xs-4 text-center">
-    //                   <a href="#">Followers</a>
-    //                 </div>
-    //                 <div className="col-xs-4 text-center">
-    //                   <a href="#">Sales</a>
-    //                 </div>
-    //                 <div className="col-xs-4 text-center">
-    //                   <a href="#">Friends</a>
-    //                 </div>
-    //               </li>
-    //               {/* Menu Footer */}
-    //               <li className="user-footer">
-    //                 <div className="pull-left">
-    //                   <a href="#" className="btn btn-default btn-flat">
-    //                     Profile
-    //                   </a>
-    //                 </div>
-    //                 <div className="pull-right">
-    //                   <a href="#" className="btn btn-default btn-flat">
-    //                     Sign out
-    //                   </a>
-    //                 </div>
-    //               </li>
-    //             </ul>
-    //           </li>
-    //           {/* Control Sidebar Toggle Button */}
-    //           <li>
-    //             <a href="#" data-toggle="control-sidebar">
-    //               <i className="fa fa-gears" />
-    //             </a>
-    //           </li>
-    //         </ul>
-    //       </div>
-    //     </nav>
-    //   </header>
-    // );
-  }
-});
+      ),
+      // Header Navbar: style can be found in header.less
+      React.createElement(
+        "nav",
+        {
+          className: "navbar navbar-static-top",
+          role: "navigation"
+        },
+        // Sidebar toggle button
+        React.createElement(
+          "a",
+          {
+            href: "#",
+            className: "sidebar-toggle",
+            "data-toggle": "offcanvas",
+            role: "button",
+            onClick: this.pushMenu
+          },
+          React.createElement(
+            "span",
+            { className: "sr-only" },
+            "Toggle navigation"
+          )
+        ),
+        // Navigation menu
+        React.createElement(
+          "div",
+          {
+            className: "navbar-custom-menu"
+          },
+          React.createElement(
+            "ul",
+            {
+              className: "nav navbar-nav"
+            }
+            /* Put custom nav bar elements here like this
+            React.createElement(
+              "li",
+              null,
+              React.createElement(
+                "a",
+                {
+                  href: "#",
+                  "data-toggle": "control-sidebar"
+                },
+                React.createElement("i", {
+                  className: "fa fa-gears"
+                })
+              )
+            ) */
+          )
+        )
+      ) // End nav
+    ); // End header
+  } // End render function
+}); // End create class
