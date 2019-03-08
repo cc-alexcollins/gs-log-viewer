bf8c9a61ee563cc5782d16cecbda5926d4473b97
Alex Collins <alex@cloudcade.com>
2019-03-07 17:15:04 -0800

    Add Timer Component
     - Enables delayed refresh
     - Multiple components can listen to and react to it without having
        the state passed down from the top level

---
+++ src/timer.js
@@ -0,0 +1,52 @@
+const TinyTimer = require("tiny-timer");
+
+let timer = null;
+let ticks = {};
+
+const callTicks = () => Object.keys(ticks).forEach(key => ticks[key]());
+
+function start(duration, callback) {
+  if (timer === null) {
+    timer = new TinyTimer();
+    timer.on("done", () => {
+      stop(); // So a new timer can be started
+      callback();
+    });
+    timer.on("tick", () => {
+      callTicks();
+    });
+
+    timer.start(duration);
+  }
+}
+
+function stop() {
+  if (timer !== null) {
+    timer.stop();
+    timer = null;
+
+    // Tick everything one last time with the timer gone
+    callTicks();
+    ticks = {};
+  }
+}
+
+function active() {
+  return timer !== null && timer.status === "running";
+}
+
+function timeLeft() {
+  return (timer !== null && timer.status === "running" ? timer.time : 0) / 1000;
+}
+
+function update(key, callback) {
+  if (timer !== null && !ticks[key]) {
+    ticks[key] = callback;
+  }
+}
+
+exports.start = start;
+exports.stop = stop;
+exports.active = active;
+exports.timeLeft = timeLeft;
+exports.update = update;
